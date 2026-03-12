b5ebc019292af5fc54d8f8fc7bf336d03bccf73a
GitEngHar <harukii1999@gmail.com>
2026-03-13 08:21:03 +0900

    commit

---
+++ app/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type Plan struct {
+	ResourceChanges []ResourceChange `json:"resource_changes"`
+}
+
+type ResourceChange struct {
+	Address string `json:"address"`
+	Change  Change `json:"change"`
+}
+
+type Change struct {
+	Actions []string `json:"actions"`
+}
+
+func main() {
+	var (
+		create, update, del int
+	)
+	if len(os.Args) < 2 {
+		fmt.Println("usage: go run analyze_plan.go plan.json")
+		os.Exit(1)
+	}
+
+	file := os.Args[1]
+
+	data, err := os.ReadFile(file)
+	if err != nil {
+		panic(err)
+	}
+
+	var plan Plan
+	err = json.Unmarshal(data, &plan)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, rc := range plan.ResourceChanges {
+		for _, action := range rc.Change.Actions {
+
+			switch action {
+			case "create":
+				create++
+			case "update":
+				update++
+			case "del":
+				del++
+			}
+
+			fmt.Printf("%s -> %s\n", rc.Address, action)
+		}
+	}
+
+	fmt.Println("\nSummary")
+	fmt.Println("create:", create)
+	fmt.Println("update:", update)
+	fmt.Println("delete:", del)
+}
